a25e3c4489d355a8422662733a4480a5fa12e6b4
Piyush Pandey <piyushpandey282@gmail.com>
2026-06-09 22:27:49 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,12 @@
+import random
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        """Randomization"""
+        n = len(nums)
+        while True:
+            candidate = random.choice(nums)
+            if nums.count(candidate)>n//2:
+                return candidate
+        
+
+        
